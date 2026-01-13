e20f10d474400577552ef0e5475dc8c45d28287e
Dark-eXe <andrewtur510@gmail.com>
2026-01-12 23:30:36 -0800

    React setup

---
+++ Source/react-ui/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Visit {" "}
+          <code>
+            <a href="https://github.com/Dark-eXe/GanAnime"  target="_blank" rel="noreferrer">github.com/Dark-eXe/GanAnime</a>
+          </code> {" "} to see current progress.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
